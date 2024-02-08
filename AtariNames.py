2a5734ab87548fe4dc8a82ced9d4ebfc43b8c130
XinJingHao <75819608+XinJingHao@users.noreply.github.com>
2024-02-08 18:55:03 +0800

    Add files via upload

---
+++ AtariNames.py
@@ -0,0 +1,112 @@
+
+
+Name = {
+	1: "Alien",
+	2: "Amidar",
+	3: "Assault",
+	4: "Asterix",
+	5: "Asteroids",
+	6: "Atlantis",
+
+	7: "BankHeist",
+	8: "BattleZone",
+	9: "BeamRider",
+	10: "Berzerk",
+	11: "Bowling",
+	12: "Boxing",
+	13: "Breakout",
+
+	14: "Centipede",
+	15: "ChopperCommand",
+	16: "CrazyClimber",
+
+	17: "Defender",
+	18: "DemonAttack",
+	19: "DoubleDunk",
+
+	20: "Enduro",
+
+	21: "FishingDerby",
+	22: "Freeway",
+	23: "Frostbite",
+
+	24: "Gopher",
+	25: "Gravitar",
+
+	26: "Hero",
+	27: "IceHockey",
+
+	28: "Jamesbond",
+
+	29: "Kangaroo",
+	30: "Krull",
+	31: "KungFuMaster",
+
+	32: "MontezumaRevenge",
+	33: "MsPacman",
+	34: "NameThisGame",
+
+	35: "Phoenix",
+	36: "Pitfall",
+	37: "Pong",
+	38: "PrivateEye",
+
+	39: "Qbert",
+
+	40: "Riverraid",
+	41: "RoadRunner",
+	42: "Robotank",
+
+	43: "Seaquest",
+	44: "Skiing",
+	45: "Solaris",
+	46: "SpaceInvaders",
+	47: "StarGunner",
+	48: "Surround",
+
+	49: "Tennis",
+	50: "TimePilot",
+	51: "Tutankham",
+
+	52: "UpNDown",
+
+	53: "Venture",
+	54: "VideoPinball",
+
+	55: "WizardOfWor",
+
+	56: "YarsRevenge",
+	57: "Zaxxon",
+}
+
+
+Action_Maps = {
+	9: [0, 1, 2, 4, 3, 6, 5, 8, 7], # BeamRider
+	13: [0,1,3,2], # Breakout
+	20 : [0,1,3,2,4,6,5,8,7], # Enduro
+	35 : [0,1,3,2,4,6,5,7], # Phoenix
+	44: [0,2,1], # Skiing
+	46: [0,1,3,2,5,4] # SpaceInvaders
+}
+
+Flip_region = { # on vertical axis
+	9: [17, 84],
+	13: [7, 84],
+	20: [0, 64],
+	35: [9, 80],
+	44: [10, 74],
+	46: [8, 84]
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
--- utils.py
+++ utils.py
@@ -1,38 +1,32 @@
-import gym
+import torch.nn.functional as F
+from torch import nn
 import torch
-import time
+import math
 
 
-
-def evaluate_policy(env, model,render=False,turns = 3):
+def evaluate_policy(env, agent, seed, turns = 3):
+    agent.q_net.eval() # 关闭NoisyNet的噪声
     scores = 0
     for j in range(turns):
-        s, done, ep_r, steps = env.reset(), False, 0, 0
+        s, info = env.reset(seed=seed)
+        done = False
         while not done:
-            if render:
-                env.render()
-                time.sleep(0.04)
-            a = model.select_action(s, evaluate=True)
-            s_prime, r, done, info = env.step(a)
-            ep_r += r
-            steps += 1
-            s = s_prime
-        scores += ep_r
-        print(ep_r)
+            a = agent.select_action(s, evaluate=True) # choose action with e-greedy = 0.01
+            s_next, r, dw, tr, info = env.step(a) # dw(dead & win): terminated, tr: truncated
+            done = (dw or tr)
+            scores += r
+            s = s_next
+    agent.q_net.train()
     return int(scores/turns)
 
 
 #You can just ignore this funciton. Is not related to the RL.
 def str2bool(v):
-    '''transfer str to bool for argparse'''
-    if isinstance(v, bool):
-        return v
-    if v.lower() in ('yes', 'True','true','TRUE', 't', 'y', '1'):
-        return True
-    elif v.lower() in ('no', 'False','false','FALSE', 'f', 'n', '0'):
-        return False
-    else:
-        raise argparse.ArgumentTypeError('Boolean value expected.')
+    '''Fix the bool BUG for argparse: transfer string to bool'''
+    if isinstance(v, bool): return v
+    if v.lower() in ('yes', 'True','true','TRUE', 't', 'y', '1', 'T'): return True
+    elif v.lower() in ('no', 'False','false','FALSE', 'f', 'n', '0', 'F'): return False
+    else: print('Wrong Input Type!')
 
 class LinearSchedule(object):
     def __init__(self, schedule_timesteps, final_p, initial_p=1.0):
@@ -57,3 +51,53 @@ class LinearSchedule(object):
     def value(self, t):
         fraction = min(float(t) / self.schedule_timesteps, 1.0)
         return self.initial_p + fraction * (self.final_p - self.initial_p)
+
+class NoisyLinear(nn.Module):
+    '''From https://github.com/Lizhi-sjtu/DRL-code-pytorch/blob/main/3.Rainbow_DQN/network.py'''
+    def __init__(self, in_features, out_features, sigma_init=0.5):
+        super(NoisyLinear, self).__init__()
+        self.in_features = in_features
+        self.out_features = out_features
+        self.sigma_init = sigma_init
+
+        self.weight_mu = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        self.weight_sigma = nn.Parameter(torch.FloatTensor(out_features, in_features))
+        self.register_buffer('weight_epsilon', torch.FloatTensor(out_features, in_features))
+
+        self.bias_mu = nn.Parameter(torch.FloatTensor(out_features))
+        self.bias_sigma = nn.Parameter(torch.FloatTensor(out_features))
+        self.register_buffer('bias_epsilon', torch.FloatTensor(out_features))
+
+        self.reset_parameters() # for mu and sigma
+        self.reset_noise() # for epsilon
+
+    def forward(self, x):
+        if self.training:
+            self.reset_noise()
+            weight = self.weight_mu + self.weight_sigma.mul(self.weight_epsilon)  # mul是对应元素相乘
+            bias = self.bias_mu + self.bias_sigma.mul(self.bias_epsilon)
+
+        else:
+            weight = self.weight_mu
+            bias = self.bias_mu
+
+        return F.linear(x, weight, bias)
+
+    def reset_parameters(self):
+        mu_range = 1 / math.sqrt(self.in_features)
+        self.weight_mu.data.uniform_(-mu_range, mu_range)
+        self.bias_mu.data.uniform_(-mu_range, mu_range)
+
+        self.weight_sigma.data.fill_(self.sigma_init / math.sqrt(self.in_features))
+        self.bias_sigma.data.fill_(self.sigma_init / math.sqrt(self.out_features))
+
+    def reset_noise(self):
+        epsilon_i = self.scale_noise(self.in_features)
+        epsilon_j = self.scale_noise(self.out_features)
+        self.weight_epsilon.copy_(torch.ger(epsilon_j, epsilon_i))
+        self.bias_epsilon.copy_(epsilon_j)
+
+    def scale_noise(self, size):
+        x = torch.randn(size)
+        x = x.sign().mul(x.abs().sqrt())
+        return x
